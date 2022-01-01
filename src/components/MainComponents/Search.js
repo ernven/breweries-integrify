fe086639e41e2fa7509511b5c5cf1c05cb9fd852
ernven <ernesto.venzano.rodriguez@myy.haaga-helia.fi>
2022-01-01 15:22:55 +0200

    Created basic search component structure.

--- src/components/MainComponents/Search.js
+++ src/components/MainComponents/Search.js
@@ -1,5 +1,31 @@
-const Search = () => (
-  <div></div>
-)
+import { useState } from 'react'
+
+import { Button, TextField, InputAdornment } from '@mui/material'
+import SearchIcon from '@mui/icons-material/Search'
+
+const Search = () => {
+  const [input, setInput] = useState('')
+
+  return (
+    <div style={{padding: '3em 4em 0'}}>
+
+      <TextField
+        value={input}
+        onChange={e => setInput(e.target.value)}
+        size='small'
+        InputProps={{startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+        ), }}
+      />
+
+      <Button size='small' onClick={() => console.log(input)}>
+        Search Breweries
+      </Button>
+
+    </div>
+  )
+}
 
 export default Search
