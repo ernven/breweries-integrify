5b63be1940f3a2a4cfafb071825c3ce66cfdc9ff
ernven <ernesto.venzano.rodriguez@myy.haaga-helia.fi>
2022-01-02 15:59:55 +0200

    files cleanup.

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import SportsBarIcon from '@mui/icons-material/SportsBar'
 const Header = () => (
   <div style={{backgroundColor: 'Gold', height: '20%'}}>
     <Typography variant='h2' sx={{padding: '0.6em 0.6em 0.2em'}}>
-      <SportsBarIcon fontSize='x-large' />Breweries
+      <SportsBarIcon fontSize='x-large' /> Breweries
     </Typography>
   </div>
 )
